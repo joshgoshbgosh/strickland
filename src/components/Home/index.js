bca9f8846c0dd01be1b275a5788f20a5469b7091
joshgoshbgosh <69131589+joshgoshbgosh@users.noreply.github.com>
2021-05-18 21:01:04 -0400

    mobile update

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import { NavLink } from 'react-router-dom';
 import {Link} from 'react-router-dom';
 import Nav from 'react-bootstrap/Nav';
+import Carousel from 'react-bootstrap/Carousel'
 import bekblkwht from './../../images/bekblkwht.jpg';
 import './index.css';
 
@@ -10,6 +11,12 @@ class Home extends Component {
     return (
       <div>
         <div className="top-intro-div">
+        <div className="mobile-home-card-div d-block d-sm-none">
+          <div className="mobile-bek-photo-div d-block d-sm-none">
+            <img className="mobile-bekblkwhthome d-block d-sm-none"src={bekblkwht}/>
+          </div>
+
+        </div>
         <div className="home-card-div d-none d-lg-block">
           <div className="bek-photo-div">
             <img className="bekblkwhthome"src={bekblkwht}/>
@@ -28,6 +35,9 @@ class Home extends Component {
             <h3>FIVE STAR RATING</h3>
           </div>
         </div>
+        </div>
+        <div className="testimonial">
+
         </div>
       </div>
     );
