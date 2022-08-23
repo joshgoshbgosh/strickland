e6c84c62e27a347ca096a511590aa979fd5269df
joshgoshbgosh <joshlaniermusic@gmail.com>
2022-08-22 22:49:33 -0400

    updates

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -20,12 +20,12 @@ class About extends Component {
           <div className="bottomDiv">
           <p className="aboutContent">
           Hi I'm Rebekah!  I was born and raised in North Carolina, where I currently
-          live with my husband and daughter.  I have been doing hair professionally since 2008.  I
+          live with my family.  I have been doing hair professionally since 2008.  I
           love doing hair because I enjoy making people feel better about themselves
           and the smile on their face is priceless.
           </p>
           </div>
-      
+
       </div>
     );
   }
